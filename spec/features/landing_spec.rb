04665e8d10c754b8a06f452c6243111fc8b2bc15
Piermaria Cosina <piermaria.cosina@gmail.com>
2012-11-29 14:52:34 +0100

    spec fix and rspec padrino

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
+
 describe "Landing" do
 
 	describe "Header" do
 
 		it "should have the content 'header'" do
-			visit 'index'
+			visit '/'
 			page.should have_content('header')
 		end
 	end
