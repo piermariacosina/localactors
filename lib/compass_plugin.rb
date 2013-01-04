80a7092e6c64eb9da8c526ba26f8a7b6b87bc9cf
Piermaria Cosina <piermaria.cosina@gmail.com>
2013-01-04 09:51:43 +0100

    fix compass for production

--- lib/compass_plugin.rb
+++ lib/compass_plugin.rb
@@ -31,7 +31,10 @@ module CompassInitializer
       end
     end
     
-    
+    if Padrino.env == :production
+       Sass::Plugin.options[:never_update] = true
+    end
+
 
     Compass.configure_sass_plugin!
     Compass.handle_configuration_change!
